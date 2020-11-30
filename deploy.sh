a647bab786dc38b35d9733f9220e8fab66efe38e
Luis Sanchez <lsjaquez22@gmail.com>
2020-11-30 10:09:48 -0700

    deploy github pages

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/med_front.git master:gh-pages
+git push -f git@github.com:lsjaquez22/med_front.git master:gh-pages
 cd -
